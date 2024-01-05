63963e86e3e1ff16f574176652408511eedc5f90
Gu-Cho <gcgchojecki@gmail.com>
2024-01-05 14:17:02 +0100

    initial commit - blinking led

---
+++ flash.sh
@@ -0,0 +1,18 @@
+# First argument should be
+# /home/gucho/Documents/Projects/arduino-uno-rust/target/avr-atmega328p/debug/arduino-uno-rust.elf
+
+set -e
+
+if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+    echo "usage: $0 <path-to-binary.elf>" >&2
+    exit 1
+fi
+
+if [ "$#" -lt 1 ]; then
+    echo "$0: Expecting a .elf file" >&2
+    exit 1
+fi
+
+# sudo -u creativcoder cargo build
+avrdude -q -C/etc/avrdude.conf -patmega328p -carduino -P/dev/ttyACM0 -D "-Uflash:w:$1:e"
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+build using
+
+> cargo build
+
+flash using
+
+./flash.sh /home/gucho/Documents/Projects/RustArduino/rust-arduino/target/avr-atmega328p/debug/rust-arduino-kek.elf
